494a0c48796f869bdfd78f31a481b40a3d92c489
Manash Kumar Mandal <manash@protonmail.com>
2016-05-21 18:20:02 +0600

    Update robotcode.ino

--- ArduinoSide/robotcode.ino
+++ ArduinoSide/robotcode.ino
@@ -1,3 +1,7 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial bt(10, 11);
+
 #define BAUD 9600
 
 #define SPEED 250
@@ -10,8 +14,8 @@ enum DIRECTION {FORWARD, LEFT, RIGHT, BACKWARD, NOWHERE};
 DIRECTION Direction = NOWHERE;
 
 //Motor pins
-int enableA = 10;
-int inputA1 = 9;
+int enableA = 9;
+int inputA1 = 3;
 int inputA2 = 8;
 
 int enableB = 5;
@@ -32,6 +36,7 @@ void forward(int speed){
 
   digitalWrite(inputB1, LOW);
   digitalWrite(inputB2, HIGH);
+
 }
 
 void left(int speed){
@@ -68,6 +73,8 @@ void stop(void){
 }
 
 void setup() {
+  pinMode(2, OUTPUT);
+  digitalWrite(2, HIGH);
   pinMode(enableA, OUTPUT);
   pinMode(enableB, OUTPUT);
   pinMode(inputA1, OUTPUT);
@@ -75,16 +82,19 @@ void setup() {
   pinMode(inputB1, OUTPUT);
   pinMode(inputB2, OUTPUT);
   Serial.begin(BAUD);
+  bt.begin(BAUD);
+  
 }
 
 void loop() {
-  if (Serial.available() > 0){
-    command = Serial.readStringUntil('\n');
+  if (bt.available() > 0){
+    command = bt.readStringUntil('\n');
+  }
 
+  
     if (command.equals("F")) forward(SPEED);
     else if (command.equals("L")) left(SPEED);
     else if (command.equals("R")) right(SPEED);
     else if (command.equals("S")) stop();
-  }
   
 }
